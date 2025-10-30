5e05db970db7fb759c88223b5262de24a5560ed3
Lukas <lukas.slehofer@gmail.com>
2025-10-30 02:02:17 +0100

    Drobná úprava mobilního zobrazení

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="cs">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Kavi Coffee - Prémiová káva s předplatným')</title>
     
