6438427e5b2c8e383b7337e1cbb7fe928f970947
Lukas <lukas.slehofer@gmail.com>
2025-11-02 19:01:28 +0100

    Překlady pro stránkování

---
+++ lang/cs/messages.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Obecné textové řetězce aplikace
+    |--------------------------------------------------------------------------
+    |
+    | Zde jsou uloženy všechny česká překlad textových řetězců používaných
+    | v aplikaci, včetně stránkování, validací a dalších běžných zpráv.
+    |
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Stránkování (Pagination)
+    |--------------------------------------------------------------------------
+    */
+    'pagination' => [
+        'previous' => '&laquo; Předchozí',
+        'next' => 'Další &raquo;',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Obecné zprávy
+    |--------------------------------------------------------------------------
+    */
+    'general' => [
+        'welcome' => 'Vítejte',
+        'home' => 'Domů',
+        'back' => 'Zpět',
+        'save' => 'Uložit',
+        'cancel' => 'Zrušit',
+        'delete' => 'Smazat',
+        'edit' => 'Upravit',
+        'create' => 'Vytvořit',
+        'update' => 'Aktualizovat',
+        'search' => 'Hledat',
+        'filter' => 'Filtrovat',
+        'submit' => 'Odeslat',
+        'close' => 'Zavřít',
+        'confirm' => 'Potvrdit',
+        'yes' => 'Ano',
+        'no' => 'Ne',
+        'loading' => 'Načítání...',
+        'success' => 'Úspěch',
+        'error' => 'Chyba',
+        'warning' => 'Varování',
+        'info' => 'Informace',
+    ],
+
+];
+
